779c8992f3f6e92fa8cfa908669b0c5db5c92e88
@technogreg4 <80175367+technogreg4@users.noreply.github.com>
2024-11-13 19:18:21 +0200

    Προσθήκη ΑΜ 8190381

---
+++ BookitMembersΑΜ.java
@@ -0,0 +1,13 @@
+public class BookitMembersΑΜ {
+    public static void main(String[] args) {
+        // Λίστα ΑΜ μελών της ομάδας "Book-it" - κάθε μέλος προσθέτει το ΑΜ του
+        int[] members_am = {
+            // Κάθε μέλος μπορεί να προσθέσει το ΑΜ του εδώ
+            8190381,
+        };
+        // Εμφάνιση κάθε ΑΜ σε νέα γραμμή
+        for (int am : members_am) {
+            System.out.println(am);
+        }
+    }
+}
